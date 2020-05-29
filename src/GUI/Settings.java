6f619cacf90a9e6b0e41930c800f0cf740488a37
jakubpartyka <j.partyka20@gmail.com>
2020-05-29 15:44:00 +0200

    settings UI progress

--- src/GUI/Settings.java
+++ src/GUI/Settings.java
@@ -5,6 +5,10 @@ import javax.swing.*;
 public class Settings {
     private JPanel settingsPanel;
     private JButton saveButton;
+    private JTextArea acceptPatterns;
+    private JTextArea ignorePatterns;
+    private JCheckBox acceptOnlyFilesThatCheckBox;
+    private JCheckBox ignoreFilesThatMatchCheckBox;
 
     /**
      * @return returns settings content panel
